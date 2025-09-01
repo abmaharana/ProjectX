c0e1260ed7ca1512d377655083361cf5d277a003
Abhishek Maharana <abhmahar@publicisgroupe.net>
2025-09-01 19:17:13 +0530

    fixes of reporting in pom

--- src/test/java/com/example/tests/runners/TestNGCucumberRunnerTest.java
+++ src/test/java/com/example/tests/runners/TestNGCucumberRunnerTest.java
@@ -6,8 +6,10 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com.example.tests.steps"},
-        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
-        tags = "@smoke"
+        plugin= {"pretty", "json:target/cucumber-reports/reports.json",
+                "json:target/cucumber-reports/cucumber.runtime.formatter.JSONFormatter"},
+        tags = "@smoke",
+        monochrome = true
 )
 public class TestNGCucumberRunnerTest extends AbstractTestNGCucumberTests {
 }
