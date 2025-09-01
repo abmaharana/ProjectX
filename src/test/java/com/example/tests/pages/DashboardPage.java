d687850468c4d860b79e1aef391a00e36d336e7e
abmaharana <112534168+abmaharana@users.noreply.github.com>
2025-09-02 00:29:34 +0530

    Update DashboardPage.java

--- src/test/java/com/example/tests/pages/DashboardPage.java
+++ src/test/java/com/example/tests/pages/DashboardPage.java
@@ -1,14 +1,14 @@
 package com.example.tests.pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 
 public class DashboardPage extends BasePage {
 
