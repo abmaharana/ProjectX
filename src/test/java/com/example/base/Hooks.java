a8ff002033610b515943d2a1d0510e0325f3856d
abmaharana <112534168+abmaharana@users.noreply.github.com>
2025-09-09 16:30:07 +0530

    Update Hooks.java

--- src/test/java/com/example/base/Hooks.java
+++ src/test/java/com/example/base/Hooks.java
@@ -1,16 +1,29 @@
 package com.example.base;
 
-import io.cucumber.java.*;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import java.net.MalformedURLException;
 
 public class Hooks {
+    private WebDriver driver;
+
     @Before
     public void setUp() throws MalformedURLException {
-        DriverFactory.getDriver();
+        driver = DriverFactory.getDriver();
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) throws MalformedURLException {
+        if (scenario.isFailed()) {
+            // Capture screenshot in bytes
+            byte[] screenshot = ((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.BYTES);
+            // Attach to Cucumber report
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
         DriverFactory.quitDriver();
     }
 }
