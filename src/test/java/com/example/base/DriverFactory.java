c251451e696756ea7b66773ea22ed922b158f3a3
abmaharana <112534168+abmaharana@users.noreply.github.com>
2025-09-09 16:29:36 +0530

    Update DriverFactory.java

--- src/test/java/com/example/base/DriverFactory.java
+++ src/test/java/com/example/base/DriverFactory.java
@@ -1,31 +1,31 @@
 package com.example.base;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterTest;
-
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Logger;
 
 public class DriverFactory {
     private static WebDriver driver;
+//    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
     private static final Logger LOGGER = Logger.getLogger(DriverFactory.class.getName());
 
     public static WebDriver getDriver() throws MalformedURLException {
+        ChromeOptions options = new ChromeOptions();
         if (driver == null) {
             String executionEnv = Config.getExecutionEnv();//System.getProperty("execution", "local");
             String hubHost = System.getProperty("HUB_HOST", "selenium-hub");
             String remoteUrl = hubHost.contains(".") ? "http://" + hubHost + ":4444/wd/hub" : "http://selenium-hub:4444/wd/hub";
 
-            LOGGER.info("Attempting to connect to Selenium Hub at: " + remoteUrl);
-
-            ChromeOptions options = new ChromeOptions();
-
             if (executionEnv.equalsIgnoreCase("docker")) {
                 try {
+                    LOGGER.info("Attempting to connect to Selenium Hub at: " + remoteUrl);
                     options.addArguments("--headless");
                     options.addArguments("--disable-dev-shm-usage");
                     options.addArguments("--no-sandbox");
@@ -41,16 +41,34 @@ public class DriverFactory {
                 }
             } else {
                 options.addArguments("--headless=new");
+                options.addArguments("--window-size=1920,1080");
                 driver = new ChromeDriver(options);
-            }
 
+            }
             driver.manage().window().maximize();
         }
         return driver;
     }
 
+    public static String captureScreenshot(WebDriver driver, String screenshotName) {
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String dirPath = System.getProperty("user.dir") + File.separator + "target" + File.separator + "screenshots" + File.separator;
+        File dir = new File(dirPath);
+        if (!dir.exists()) {
+            dir.mkdirs(); // create folder if not exists
+        }
+        String filePath = dirPath + screenshotName.replaceAll("[^a-zA-Z0-9]", "_") + "_" + timestamp + ".png";
+        try {
+            Thread.sleep(2000);
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(screenshot, new File(filePath));
+            System.out.println("Screenshot saved at: " + filePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return filePath;
+    }
 
-    @AfterTest
     public static void quitDriver() {
             if (driver != null) {
                 driver.quit();
